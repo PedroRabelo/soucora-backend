3d41f085733684fa611bef5c88d7808e00815ea1
pedro <pnarabelo@gmail.com>
2022-10-26 11:48:17 -0300

    gitignore

--- src/resources/users/users.controller.ts
+++ src/resources/users/users.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -7,6 +8,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @IsPublic()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
---
+++ to-do.md
@@ -0,0 +1,9 @@
+### FEATURES
+
+## ACCOUNT
+[x] Autenticar usuário
+[x] Criar usuário
+[] No login validar se usuário ativo
+[] Enviar e-mail de confirmação com activationKey
+[] Ativar usuário
+[] Esqueceu a senha
